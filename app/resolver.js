ebfbc858ca3f6901d8b4db2bf044c0e37738b453
Jared Hanson <jaredhanson@gmail.com>
2018-04-10 08:49:57 -0700

    Revert host resolution.

--- app/resolver.js
+++ app/resolver.js
@@ -7,7 +7,7 @@ exports = module.exports = function(IoC, webfinger, lrdd, mx, logger) {
   
   return Promise.resolve(resolver)
     .then(function(resolver) {
-      var components = IoC.components('http://schemas.authnomicon.org/js/id/EntityResolutionProtocol');
+      var components = IoC.components('http://schemas.authnomicon.org/js/ns/ResolutionProtocol');
       
       return Promise.all(components.map(function(comp) { return comp.create(); } ))
         .then(function(protocols) {
@@ -18,22 +18,6 @@ exports = module.exports = function(IoC, webfinger, lrdd, mx, logger) {
           
           //resolver.use(webfinger);
           resolver.use(lrdd);
-        })
-        .then(function() {
-          return resolver;
-        });
-    })
-    .then(function(resolver) {
-      var components = IoC.components('http://schemas.authnomicon.org/js/id/HostResolutionProtocol');
-      
-      return Promise.all(components.map(function(comp) { return comp.create(); } ))
-        .then(function(protocols) {
-          protocols.forEach(function(protocol, i) {
-            resolver.use(protocol);
-            logger.info('Loaded host-based identity resolution protocol: ' + components[i].a['@name']);
-          });
-          
-          //resolver.use(mx);
         })
         .then(function() {
           return resolver;
